0940a38088b83928523dd23380394737bf5cf536
Bang <jazzdosa@kaist.ac.kr>
2022-07-12 07:44:35 +0900

    slime_emoji

--- week2flutter/lib/Global/MyGame.dart
+++ week2flutter/lib/Global/MyGame.dart
@@ -32,6 +32,8 @@ class tapSlimeComponent extends SpriteAnimationComponent with Tappable{
 
 late var heartspr;
 late var transspr;
+late var upsetspr;
+
 
 
 class Slime extends SpriteAnimationComponent with Tappable{
@@ -45,12 +47,22 @@ class Slime extends SpriteAnimationComponent with Tappable{
   @override
   bool onTapDown(TapDownInfo event){
     try{
-      tapped = 1;
-      print("slime tapped!");
-      Future.delayed(const Duration(milliseconds: 2000), (){
-        tapped = 0;
-        print("tap finished");
-      });
+      if(tapped == 0) { // 눌린 적이 없으면
+        tapped = 1;
+        print("slime tapped!");
+        Future.delayed(const Duration(milliseconds: 2000), () {
+          if(tapped == 1) tapped = 0;
+          print("tap finished");
+        });
+      }
+      else if(tapped == 1){ // 눌린 상태에서 또 누르면
+        tapped = 2;
+        print("slime double tapped!");
+        Future.delayed(const Duration(milliseconds: 2000), () {
+          tapped = 0;
+          print("tap finished");
+        });
+      }
       return true;
     } catch(error){
       print(error);
@@ -73,7 +85,7 @@ class MyGame extends FlameGame with HasTappables{
   var spList = <SpriteSheet>[];
   var rAList = <SpriteAnimation>[]; // 오른쪽 애니메이션들 모음
   var lAList = <SpriteAnimation>[]; // 왼쪽 애니메이션들 모음
-  var path = [[1, 0], [0, 1], [-1, 0], [0, -1]]; // 방향
+  var path = [[1, 0], [0, 1], [-1, 0], [0, -1], [0.707, 0.707], [0.707, -0.707], [-0.707, 0.707], [-0.707, -0.707]]; // 방향
 
   Slime slime = Slime();
   var pointText = TextComponent();
@@ -97,6 +109,7 @@ class MyGame extends FlameGame with HasTappables{
     ////heart/////
     heartspr = await loadSprite('heart2.png');
     transspr = await loadSprite('transp.png');
+    upsetspr = await loadSprite('upset.png');
 
 
     ////////// BACKGROUND //////////////
@@ -204,12 +217,12 @@ class MyGame extends FlameGame with HasTappables{
     for(int i=0; i < slimeList.length; i++){
       Slime slime = slimeList[i];
       if(slimeList[i].dist<0){
-        slimeList[i].dir = Random().nextInt(4);
-        if(slimeList[i].dir==0){
-          slime.animation = rAList[slimeList[i].type ?? 0]; // 왼쪽무빙
+        slimeList[i].dir = Random().nextInt(8);
+        if(slimeList[i].dir==0 || slimeList[i].dir==4 || slimeList[i].dir==5){
+          slime.animation = rAList[slimeList[i].type ?? 0]; // 오른쪽무빙
         }
-        if(slimeList[i].dir==2){
-          slime.animation = lAList[slimeList[i].type ?? 0]; // 오른쪽무빙
+        if(slimeList[i].dir==2 || slimeList[i].dir==6 || slimeList[i].dir==7){
+          slime.animation = lAList[slimeList[i].type ?? 0]; // 왼쪽무빙
         }
         slimeList[i].dist=(Random().nextDouble()*100).toInt();
       }
@@ -240,8 +253,13 @@ class MyGame extends FlameGame with HasTappables{
       if(slime.tapped==0){ // 탭되지 않았을 때
         slime.heart.sprite = transspr; // 말풍선 투명
       }
-      else{
+      else if(slime.tapped == 1){
         slime.heart.sprite = heartspr; // 하트 띄워지고 가만히 서있게
+        slime.heart.size = Vector2(30, 30);
+        slime.heart.position = Vector2(slime.x+65, slime.y);
+      }
+      else{
+        slime.heart.sprite = upsetspr; // 하트 띄워지고 가만히 서있게
         slime.heart.size = Vector2(30, 30);
         slime.heart.position = Vector2(slime.x+65, slime.y);
       }
